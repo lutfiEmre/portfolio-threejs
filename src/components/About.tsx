669d52142f39deba8f464c7affb20888611cdf0a
Emre Lütfi <mail@emrelutfi.com>
2025-05-04 21:32:41 +0300

    asd

---
+++ src/components/About.tsx
@@ -0,0 +1,16 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          Hi, I'm Emre Lutfi-a front-end developer with a passion for user experience. While writing scalable interfaces with Next.js, React, TypeScript and TailwindCSS, I aim to deliver fluid and memorable experiences to visitors by bringing interaction to 3D scenes with tools like Framer Motion and Three.js. I care about type safety and code readability, and equally value performance, design integrity, and accessibility. Working on real-world problems while studying Software Engineering nourishes me; I see every bug report as an opportunity to learn, and I retain the knowledge gained through detailed documentation.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Career.tsx
@@ -0,0 +1,128 @@
+import "./styles/Career.css";
+
+const Career = () => {
+  return (
+    <div className="career-section section-container">
+      <div className="career-container">
+        <h2>
+          My career <span>&</span>
+          <br /> experience
+        </h2>
+        <div className="career-info">
+          <div className="career-timeline">
+            <div className="career-dot"></div>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>Front-End Lead</h4>
+                <h5> Activate Earth</h5>
+              </div>
+              <h3>SEP 2024 - APRIL 2025</h3>
+            </div>
+            <p>
+              Developed Activ8Earth, an interactive crypto coin platform
+              where users earn money by daily interactions, using Next.js,
+              React, TypeScript, and RainbowKit
+              <br/>
+              Designed and implemented the landing page, integrated APIs
+              with AWS, managed authentication via Cognito, and utilized
+              Wagmi for state management.
+              Assa Tech / Front-End Lea
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>Front-End Lead</h4>
+                <h5> Assa Tech</h5>
+              </div>
+              <h3> APR 2024 – OCT 2024</h3>
+            </div>
+            <p>
+              Delivered 10+ fully functional products, replicating Figma designs
+              with precision in both appearance and functionality.
+              <br/>
+              Took full ownership of the design-to-deployment process, including
+              wireframing, prototyping, and testing.
+              <br/>
+              Lead the development of the Applicant Tracking System (ATS) project
+              using React, TypeScript, and TailwindCSS, ensuring on-time delivery
+              aligned with client expectations.
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>Lead Front-End Developer</h4>
+                <h5>TurkStudentCo</h5>
+              </div>
+              <h3> SEP 2024 - DEC 2024</h3>
+            </div>
+            <p>
+              Lead the front-end development of education platform projects
+              using Next.js and React, alongside Figma for design implementation.
+              Developed mini e-commerce applications by meeting project
+              requirements and ensuring a seamless user experience
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4> Front-End Developer</h4>
+                <h5> User Metrics</h5>
+              </div>
+              <h3> FEB 2024 – AUG 2024</h3>
+            </div>
+            <p>
+              Built a mini e-commerce website with login and registration systems
+              using React, Next.js, and TypeScript, ensuring it matched Figma
+              designs perfectly.
+              <br/>
+              Gained experience in corporate relations, software development,
+              and manipulative marketing strategies essential for
+              entrepreneurship
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4> UX/UI Designer</h4>
+                <h5> inowhat</h5>
+              </div>
+              <h3> MAR 2023 - JULY 2023</h3>
+            </div>
+            <p>
+              Designed Meditouch, an IoT device and app for real-time elderly
+              care monitoring, using Figma for wireframes, prototypes, and user
+              testing.
+              <br/>
+              Created landing pages and tailored business plans to enhance
+              customer value and drive sales
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4> Front-End Developer</h4>
+                <h5> DataFex</h5>
+              </div>
+              <h3> MAR 2023 - JULY 2023</h3>
+            </div>
+            <p>
+              Developed a fully functional website from scratch using HTML, CSS,
+              and front-end technologies, ensuring seamless user experience.
+              Managed system administration tasks, including Plesk, vCenter, and
+              WHMCS, for efficient website operations.
+              <br/>
+              Enhanced skills in corporate relations, software development, and
+              entrepreneurial marketing strategies
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Career;
